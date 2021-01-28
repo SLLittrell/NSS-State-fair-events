a5c1595191c2bbdb0aa8b32472f68004a425cc85
Stacey Littrell <stacey.littrell@gmail.com>
2021-01-28 12:40:05 -0600

    Added event listener to RideTicketHolder.js, targeted class rides, redered each 'ticket' to DOM

--- scripts/rides/RideTicketHolders.js
+++ scripts/rides/RideTicketHolders.js
@@ -1,8 +1,13 @@
 const contentTarget = document.querySelector(".rides")
 const eventHub = document.querySelector("#state-fair")
 
-export const RideTicketHolders = () => {
-    eventHub.addEventListener("", customEvent => {
+// export const RideTicketHolders = () => {
+    eventHub.addEventListener("rideTicketPurchased", customEvent => {
+        if(customEvent.detail.purchasedTicket === "Ticket") {
+
+            // console.log("heard")
+            contentTarget.innerHTML +=`<div class="person rider"></div>`
+        }
     })
-}
+// }
 
